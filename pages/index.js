0e3755dec151492b91f51a808919c2f6a7ef7878
Noorulla-paktolus <138501957+ANoorulla@users.noreply.github.com>
2024-07-17 23:11:31 +0530

    changed title name

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ function HomePage(props) {
   return (
     <Fragment>
       <Head>
-        <title>Max&apos;s Blog</title>
+        <title>Noor&apos;s Blog</title>
         <meta
           name="description"
           content="I post about programming and web development."
